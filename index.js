60eea59866815bcf39a04d790ed88f69cfa07216
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-11-30 21:49:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    let target = document.getElementById('text');
+    target.innerHTML = "This is really cool!";
+}) 
